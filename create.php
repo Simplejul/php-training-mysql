ccbe232da36e7e772a13891372fcdbe6b27d3c1a
Simplejul <simplejul@protonmail.com>
2018-08-09 14:59:57 +0200

    next

--- create.php
+++ create.php
@@ -6,7 +6,7 @@
 	<link rel="stylesheet" href="css/basics.css" media="screen" title="no title" charset="utf-8">
 </head>
 <body>
-	<a href="/php-pdo/read.php">Liste des données</a>
+	<a href="read.php">Liste des données</a>
 	<h1>Ajouter</h1>
 	<form action="" method="post">
 		<div>
@@ -39,5 +39,70 @@
 		</div>
 		<button type="submit" name="button">Envoyer</button>
 	</form>
+
+	<?php
+		// $name=$_POST['name'];
+		// $difficulty=$_POST['difficulty'];
+		// $distance=$_POST['distance'];
+		// $duration=$_POST['duration'];
+		// $height=$_POST['height_difference'];
+		if (isset($_POST)){
+
+			$name=$_POST['name'];
+			$difficulty=$_POST['difficulty'];
+			$distance=$_POST['distance'];
+			$duration=$_POST['duration'];
+			$height=$_POST['height_difference'];
+
+			// $pdo = new PDO('mysql:host=localhost;dbname=reunion_island','root','toor');
+
+			// $statement = $pdo->prepare("INSERT INTO hiking(name,difficulty,distance,duration,height_difference) 
+			// VALUES(:name, :difficulty, :distance, :duration, :height_difference)");
+			// $statement->execute([
+			// 	"name" => $name,
+			// 	"difficulty" => $difficulty,
+			// 	"distance" => $distance,
+			// 	"duration" => $duration,
+			// 	"height_difference" => $height
+			// ]);
+
+			// $pdo = new PDO('mysql:host=localhost;dbname=reunion_island','root','toor',array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+			//   $sql = 'INSERT INTO hiking(name,difficulty,distance,duration,height_difference)
+			//    VALUES ("$name","facile","32","03:00:00","33")';
+			//   $req = $pdo -> query($sql);
+			//   $req -> closeCursor();
+
+			echo $_POST['name'];
+			echo gettype($duration);
+			$servername = "localhost";
+			$database = "reunion_island";
+			$username = "root";
+			$password = "toor";
+
+			// Create connection
+
+			$conn = mysqli_connect($servername, $username, $password, $database);
+
+			// Check connection
+
+			if (!$conn) {
+				die("Connection failed: " . mysqli_connect_error());
+			}
+			
+			echo "Connected successfully";
+			
+			$sql = "INSERT INTO hiking (name,difficulty,distance,duration,height_difference) VALUES ($name, $difficulty, $distance, $duration, $height)";
+			
+			if (mysqli_query($conn, $sql)) {
+				echo "New record created successfully";
+			} else {
+				echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+			}
+			mysqli_close($conn);
+		}
+
+
+	?>
+
 </body>
 </html>
--- read.php
+++ read.php
@@ -8,7 +8,33 @@
   <body>
     <h1>Liste des randonnées</h1>
     <table>
+      
       <!-- Afficher la liste des randonnées -->
+      <caption>Réunion rando</caption>
+      <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Difficulty</th>
+          <th scope="col">Distance</th>
+          <th scope="col">Duration</th>
+          <th scope="col">Height Difference</th>
+      </tr>
+        <?php
+          $pdo = new PDO('mysql:host=localhost;dbname=reunion_island','root','toor',array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+          $sql = 'SELECT * FROM hiking';
+          $req = $pdo -> query($sql);
+          while ($row = $req -> fetch()){
+              echo '<tr>
+              <td>'.$row['name'].'</td>
+              <td>'.$row['difficulty'].'</td>
+              <td>'.$row['distance'].'</td>
+              <td>'.$row['duration'].'</td>
+              <td>'.$row['height_difference'].'</td>
+          </tr>';
+          };
+          $req -> closeCursor();
+
+        ?>
     </table>
+
   </body>
 </html>
